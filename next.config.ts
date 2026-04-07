ba971f7a7ceb0fe71576bbce271cce59b5bde65c
Dmytro Larin <d.larin@mascode.com>
2026-04-07 21:15:41 +0300

    Add some small updates for next deployment

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {turbo: undefined},
   output: 'export',
   basePath: '/psy',
   trailingSlash: true,
   images: {
     unoptimized: true,
-  },
-  webpack: (config: any, {dev}: any) => {
-    if (dev) {
-      config.cache = false;
-    }
-    return config;
   },
 };
 
